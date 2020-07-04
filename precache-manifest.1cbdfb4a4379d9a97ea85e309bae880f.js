ffc4b9c54447656580f672bcc67098d2f25a4456
Jourden <jourdenrai@gmail.com>
2020-07-04 03:38:05 -0500

    Updates

---
+++ precache-manifest.1cbdfb4a4379d9a97ea85e309bae880f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebacb0c2f7207afb0ee307f24b33db2b",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "2025a9a99888ea27625b",
+    "url": "/react-restaurant/static/css/main.dbcf9fd4.chunk.css"
+  },
+  {
+    "revision": "5b0c7dbb5608d5cd9791",
+    "url": "/react-restaurant/static/js/2.34ddcfd2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-restaurant/static/js/2.34ddcfd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2025a9a99888ea27625b",
+    "url": "/react-restaurant/static/js/main.4b8a7763.chunk.js"
+  },
+  {
+    "revision": "a6b598b921fae64296e8",
+    "url": "/react-restaurant/static/js/runtime-main.c7cd40a3.js"
+  },
+  {
+    "revision": "ed93c211ebe1e8da213842da61811cca",
+    "url": "/react-restaurant/static/media/bg.ed93c211.png"
+  },
+  {
+    "revision": "24194d4ae665c1aeccc9c5065967a73c",
+    "url": "/react-restaurant/static/media/blackened-shrimp.24194d4a.jpg"
+  },
+  {
+    "revision": "624375bda04dd3bd5736a7f18321bdac",
+    "url": "/react-restaurant/static/media/city.624375bd.jpg"
+  },
+  {
+    "revision": "d4252fcb328cdbf2117ffb2b943bf629",
+    "url": "/react-restaurant/static/media/fish-poboy.d4252fcb.jpg"
+  },
+  {
+    "revision": "527c4831d3a37e0a58f341eb935924b9",
+    "url": "/react-restaurant/static/media/fishnchips.527c4831.jpg"
+  },
+  {
+    "revision": "4555711aa668e5dad8e86a61d7882bbe",
+    "url": "/react-restaurant/static/media/grilledfish.4555711a.jpg"
+  },
+  {
+    "revision": "8775bb5e5360fdc191ce5998d1eb7bd6",
+    "url": "/react-restaurant/static/media/lobstertail.8775bb5e.jpg"
+  },
+  {
+    "revision": "d98c8d8f76aa5a028837aa43f1f57ee3",
+    "url": "/react-restaurant/static/media/oysters.d98c8d8f.jpg"
+  },
+  {
+    "revision": "cbaed1d29304916cfff7367faa3338f3",
+    "url": "/react-restaurant/static/media/shrimp-bg.cbaed1d2.png"
+  },
+  {
+    "revision": "57228e79304ec2c1ce0a5393a80162ec",
+    "url": "/react-restaurant/static/media/shrimp-pasta.57228e79.jpg"
+  },
+  {
+    "revision": "8b712d6f7a3439577bca75017fc601c1",
+    "url": "/react-restaurant/static/media/shrimpnchips.8b712d6f.jpg"
+  }
+]);
